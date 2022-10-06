d7cc49dc86cbe3886b9a2b049163e2dea7aa5baa
Ivan Gr <of_2000@mail.ru>
2022-10-07 00:16:11 +0400

    Homework

---
+++ 01_Task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+using static System.Console;
+Clear();
+WriteLine("***Программа для подсчета количества положительных чисел, введенных пользователем***");
+WriteLine("Введите числа, используя пробел или запятую в качестве разделителя");
+string[] array = ReadLine()!.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+int x = countNumbers(array);
+WriteLine($"Введено {x} положительных чисел");
+
+
+
+
+
+
+
+
+int countNumbers(string[] array4)
+{
+    int numbers = 0;
+    int[] array5 = new int[array4.Length];
+    
+    for (int i = 0; i < array4.Length; i++)
+    {
+        array5[i] = Convert.ToInt32(array4[i]);
+    }
+    
+    for (int i = 0; i < array5.Length; i++)
+    {
+        if(array5[i] > 0) numbers++;
+    }
+    return numbers;
+}
---
+++ 02_Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+using static System.Console;
+Clear();
+WriteLine("*** Находим точки пересечения графиков уравнений y = k1*x + b1, y = k2*x + b2 ***");
+WriteLine("Введите значения k1, b1, k2 и b2, используя пробел или запятую в качестве разделителя: ");
+string[] array = ReadLine()!.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+double[] data = ReadString(array);
+double x = (data[3] - data[1])/(data[0] - data[2]);
+double y = data[0]*x + data[1];
+WriteLine($"Местом пересечения графиков уравнений y = {data[0]}*x + {data[1]}, y = {data[2]}*x + {data[3]} является точка с координатами ({x}; {y})");
+
+
+
+
+
+
+
+double [] ReadString(string[] input)
+{
+    double[] result = new double [input.Length];
+    for (int i = 0; i < input.Length; i++)
+    {
+        result[i] = Convert.ToInt32(input[i]);
+    }
+    return result;
+}
